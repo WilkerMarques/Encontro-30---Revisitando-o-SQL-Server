b915ab71741949919a39ab670b0cba7e81b8ac16
Wilker Marques <90647686+WilkerMarques@users.noreply.github.com>
2021-10-27 16:45:23 -0400

    Delete psgspringboot directory

--- psgspringboot/src/main/java/com/example/demo/repository/IDepartamentoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.model.Departamento;
-
-public interface IDepartamentoRepository extends JpaRepository<Departamento, Integer> {
-
-}
